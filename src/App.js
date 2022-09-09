09173d7b4038bf503f6eef9709d1686ba7b7cf4a
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-09-09 15:23:11 -0400

    flopy bird

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import { useEffect, useState } from 'react';
 const BIRD_SIZE         = 20;
 const GAME_WIDTH        = 500;
 const GAME_HEIGHT       = 500;
-const GRAVITY           = 6;
-const JUMP_HEIGHT       = 100;
-const OBSTACLE_WIDTH    = 40
+const GRAVITY           = 3;
+const JUMP_HEIGHT       = 50;
+const OBSTACLE_WIDTH    = 40;
+const OBSTACLE_GAP      = BIRD_SIZE * 5;
 
 function App() {
   const [birdPosition, setBirdPosition] = useState(250);
   const [gameHasStarted, setGameHasStarted] = useState(false)
-  const [obstacleHeight, setObstacleHeight] = useState(100)
+  const [obstacleHeight, setObstacleHeight] = useState(200)
   const [obstacleLeft, setObstacleLeft] = useState(GAME_WIDTH - OBSTACLE_WIDTH)
+  const [score, setScore] = useState(0)
 
+  const bottomObstacleHeight = GAME_HEIGHT - OBSTACLE_GAP - obstacleHeight;
 
   useEffect(() => {
     let timeId;
-
     if (gameHasStarted && birdPosition < GAME_HEIGHT - BIRD_SIZE) {
       timeId = setInterval(() => {
         setBirdPosition(birdPosition => birdPosition + GRAVITY)
@@ -27,7 +29,38 @@ function App() {
     return () => {
       clearInterval(timeId);
     };
-  }, [birdPosition, gameHasStarted])
+  }, [birdPosition, gameHasStarted]);
+
+useEffect(() => {
+  let obstacleId;
+  if (gameHasStarted && obstacleLeft >= -OBSTACLE_WIDTH) {
+    obstacleId = setInterval(() => {
+      setObstacleLeft((obstacleLeft) => obstacleLeft - 5);
+    }, 24);
+    return() => {
+      clearInterval(obstacleId)
+    }
+  }
+    else {
+      setObstacleLeft(GAME_WIDTH - OBSTACLE_WIDTH);
+      setObstacleHeight(Math.floor(Math.random() * (GAME_HEIGHT - OBSTACLE_GAP)));
+      setScore (score => score +1 )
+    }
+}, [gameHasStarted, obstacleLeft]);
+
+useEffect(() => {
+  const hasCollidedWithTopObstacle =
+    birdPosition >= 0 && birdPosition < obstacleHeight;
+  const hasCollidedWithBottomObstacle =
+    birdPosition <= 500 & birdPosition > 500 - bottomObstacleHeight;
+  if (obstacleLeft >= 0 &&
+      obstacleLeft <= OBSTACLE_WIDTH &&
+      (hasCollidedWithTopObstacle || hasCollidedWithBottomObstacle)
+      )  {
+      setGameHasStarted(false);
+      setScore(0)
+    }
+},  [birdPosition, obstacleHeight, bottomObstacleHeight, obstacleLeft])
 
   const handleClick = () => {
     let newBirdPosition = birdPosition - JUMP_HEIGHT;
@@ -49,10 +82,15 @@ function App() {
           height={obstacleHeight}
           left={obstacleLeft}
         />
-
+         <Obstacle
+          top={GAME_HEIGHT - (obstacleHeight + bottomObstacleHeight)}
+          width={OBSTACLE_WIDTH}
+          height={bottomObstacleHeight}
+          left={obstacleLeft}
+        />
         <Bird size={BIRD_SIZE} top={birdPosition} />
       </GameBox>
-
+      <span>{score}</span>
     </Div>
   );
 }
@@ -71,11 +109,18 @@ const Div = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
+  & span {
+    color: white;
+    font-size: 24px;
+    /* background-color: black; */
+    position: absolute;
+  }
 `
 const GameBox = styled.div`
   height: ${(props) => props.height}px;
   width: ${(props) => props.width}px;
   background-color: blue;
+  overflow: hidden;
 `
 const Obstacle = styled.div`
   position: relative;
