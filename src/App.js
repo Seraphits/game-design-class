6f32a726e341b0b99a96bff20e62f9a5f94b7dab
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-09-22 14:19:39 -0400

    link game pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GraphQL001 from './AddingData/GraphQL/001'
 import FlashCard from './Game/FlashCards/001/FlashCardsPage'
 import FloppyBird from './Game/FloppyBird/001/FloppyBird'
 import MemoryGamePage from './Game/Memory/001/MemoryGamePage'
+import MemoryGamePage2 from './Game/Memory/002'
 import Header from './Layout/Header'
 import AddingData from './Pages/AddingData'
 import GamesList from './Pages/GamesList'
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/games/floppybird'><FloppyBird/></Route>
         <Route path='/games/flashcards'><FlashCard/></Route>
         <Route path='/games/memory/001'><MemoryGamePage/></Route>
+        <Route path='/games/memory/002'><MemoryGamePage2/></Route>
         <Route path='/games'><GamesList/></Route>
         <Route path='/addingdata/GraphQL/001'><GraphQL001/></Route>
         <Route path='/addingdata'><AddingData/></Route>
---
+++ src/Game/Memory/002/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      Merory Game 2
+    </div>
+  )
+}
+
+export default index
