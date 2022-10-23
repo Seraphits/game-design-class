e738e0b287e261c25669b185a239d3cf54ffb492
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-10-23 04:38:01 -0400

    setup

---
+++ src/Game/Tabla/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Tabla = () => {
+  return (
+    <div>Tabla</div>
+  )
+}
+
+const Tabla_Sounds = [
+  {
+    "english": "TA",
+    "hindi": "ता"
+  },
+  {
+    "english": "NA",
+    "hindi": "ना"
+  },
+  {
+    "english": "TIN",
+    "hindi": "तिन"
+  },
+  {
+    "english": "TU",
+    "hindi": "तू"
+  },
+  {
+    "english": "TE",
+    "hindi": "ते"
+  },
+  {
+    "english": "TETE",
+    "hindi": "तेटे"
+  },
+  {
+    "english": "KA",
+    "hindi": "का"
+  },
+  {
+    "english": "KI",
+    "hindi": "की"
+  },
+  {
+    "english": "KE",
+    "hindi": "के"
+  },
+  {
+    "english": "KATH",
+    "hindi": "कथ"
+  },
+  {
+    "english": "GA",
+    "hindi": "ग"
+  },
+  {
+    "english": "GHA",
+    "hindi": "घ"
+  }
+]
