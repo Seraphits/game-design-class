f489c3ec624e9753cc0dc87727a555ad254a6bdf
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-10-30 14:24:56 -0400

    Change Qu type

--- src/Game/Tabla/index.js
+++ src/Game/Tabla/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import TablaData from "./Data";
+import { FinalResults } from './FinalResults';
 import { Header } from './Header';
 
 export const Tabla = () => {
@@ -13,6 +14,10 @@ export const Tabla = () => {
  const [score, setScore] = useState(34);
  const [currentSounds1, setCurrentSound1] = useState(0);
  const [currentSounds2, setCurrentSound2] = useState(1);
+ const [currentSounds3, setCurrentSound3] = useState(2);
+ const [currentSounds4, setCurrentSound4] = useState(3);
+ const [currentSounds5, setCurrentSound5] = useState(4);
+ const [currentSounds6, setCurrentSound6] = useState(5);
   return (
     <div>
       {/* Header */}
@@ -22,32 +27,56 @@ export const Tabla = () => {
 
       { showFinalResults ? (
         // {/* Final Results Card */}
-        <div>
+        <FinalResultsCard>
           <h2>Final Results</h2>
           <h3>1 out of 5 correct - (20%)</h3>
           <button>play again</button>
-        </div>)
+        </FinalResultsCard>)
     :
         (
           // {/* Question card */}
-          <div className='question-card'>
+          <QuestionCard>
             <h2>Question 1</h2>
-            <ul>
-              <li><div>{sounds[currentSounds1].hindi}</div></li>
-              <li><div>{sounds[currentSounds2].hindi}</div></li>
-            </ul>
-            <ul>
-              <li><div>Same</div></li>
-              <li><div>Different</div></li>
-            </ul>
-          </div>
+            <Question>
+              {sounds[currentSounds1].hindi}
+            </Question>
+            <Opions>
+              <Opion ><div>{sounds[currentSounds1].hindi}</div></Opion >
+              <Opion ><div>{sounds[currentSounds2].hindi}</div></Opion >
+              <Opion ><div>{sounds[currentSounds3].hindi}</div></Opion >
+              <Opion ><div>{sounds[currentSounds4].hindi}</div></Opion >
+              <Opion ><div>{sounds[currentSounds5].hindi}</div></Opion >
+              <Opion ><div>{sounds[currentSounds6].hindi}</div></Opion >
+            </Opions>
+          </QuestionCard>
         )
-    }
-
-
-
-
+      }
     </div>
   )
 }
 
+const QuestionCard = styled.div`
+  background-color: lightgreen;
+  padding: 1rem;
+  margin: 2rem;
+`;
+const Question = styled.div`
+  /* list-style: none; */
+  text-align: center;
+`;
+const Opions = styled.ul`
+  list-style: none;
+  display: grid;
+`;
+const Opion = styled.li`
+  background-color: aliceblue;
+  padding: 1rem;
+  margin: 1rem;
+  border: 3px solid green;
+`;
+
+const FinalResultsCard = styled.div`
+  background-color: lightcoral;
+  padding: 1rem;
+  margin: 2rem;
+`;
