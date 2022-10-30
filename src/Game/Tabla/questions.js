f2e9936ccd023930d54aff1d903af3c87bb5a0a6
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-10-30 16:15:18 -0400

    Questions list

---
+++ src/Game/Tabla/questions.js
@@ -0,0 +1,92 @@
+const Tabla_Questions = [
+  {
+    question: "ता",
+    options: [
+      {id: 0, option: "तिन", isCorrect: false},
+      {id: 1, option: "तेटे", isCorrect: false},
+      {id: 2, option: "ता", isCorrect: true},
+      {id: 3, option: "का", isCorrect: false},
+      {id: 4, option: "घ", isCorrect: false},
+      {id: 5, option: "ते", isCorrect: false}
+    ]
+  },
+  {
+    question: "TU",
+    options: [
+      {id: 0, option: "तू", isCorrect: True},
+      {id: 1, option: "तेटे", isCorrect: false},
+      {id: 2, option: "ना", isCorrect: false},
+      {id: 3, option: "घ", isCorrect: false},
+      {id: 4, option: "ग", isCorrect: false},
+      {id: 5, option: "का", isCorrect: false}
+    ]
+  },
+  {
+    question: "का",
+    options: [
+      {id: 0, option: "KI", isCorrect: false},
+      {id: 1, option: "TE", isCorrect: false},
+      {id: 2, option: "KE", isCorrect: false},
+      {id: 3, option: "NA", isCorrect: false},
+      {id: 4, option: "TA", isCorrect: false},
+      {id: 5, option: "KA", isCorrect: true}
+    ]
+  },
+  {
+    question: "TU",
+    options: [
+      {id: 0, option: "ते", isCorrect: false},
+      {id: 1, option: "ना", isCorrect: false},
+      {id: 2, option: "ता", isCorrect: false},
+      {id: 3, option: "का", isCorrect: false},
+      {id: 4, option: "तू", isCorrect: true},
+      {id: 5, option: "कथ", isCorrect: false}
+    ]
+  },
+  {
+    question: "कथ",
+    options: [
+      {id: 0, option: "का", isCorrect: false},
+      {id: 1, option: "ग", isCorrect: false},
+      {id: 2, option: "कथ", isCorrect: true},
+      {id: 3, option: "ते", isCorrect: false},
+      {id: 4, option: "तेटे", isCorrect: false},
+      {id: 5, option: "ता", isCorrect: false}
+    ]
+  },
+  {
+    question: "TIN",
+    options: [
+      {id: 0, option: "TU", isCorrect: false},
+      {id: 1, option: "TE", isCorrect: false},
+      {id: 2, option: "KATH", isCorrect: false},
+      {id: 3, option: "GHA", isCorrect: false},
+      {id: 4, option: "TIN", isCorrect: true},
+      {id: 5, option: "TA", isCorrect: false}
+    ]
+  },
+  // {
+  //   question: "",
+  //   options: [
+  //     {id: 0, option: "", isCorrect: false},
+  //     {id: 1, option: "", isCorrect: false},
+  //     {id: 2, option: "", isCorrect: false},
+  //     {id: 3, option: "", isCorrect: false},
+  //     {id: 4, option: "", isCorrect: false},
+  //     {id: 5, option: "", isCorrect: false}
+  //   ]
+  // },
+  {
+    question: "घ",
+    options: [
+      {id: 0, option: "तेटे", isCorrect: false},
+      {id: 1, option: "तू", isCorrect: false},
+      {id: 2, option: "घ", isCorrect: true},
+      {id: 3, option: "ते", isCorrect: false},
+      {id: 4, option: "के", isCorrect: false},
+      {id: 5, option: "ग", isCorrect: false}
+    ]
+  }
+]
+
+export default Tabla_Sounds;
