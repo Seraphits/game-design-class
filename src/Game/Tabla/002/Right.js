f01f0e11bd33416c3ccc5237aac9fa615a3316cf
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-11-12 09:40:24 -0500

    Loop to new Qu R

--- src/Game/Tabla/002/Right.js
+++ src/Game/Tabla/002/Right.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { QuestionComp } from './Question';
 
 function Right() {
+  const [showQuestion, setShowQuestion] = useState(false);
+  const handleClick = () => {
+    console.log("startScreen");
+    // makeQu()
+    setShowQuestion(true);
+  }
   return (
-    <div>Right</div>
+    <div>
+        {showQuestion ? (
+          <QuestionComp/>
+        ) : (
+          <>
+            Right
+            <button onClick={() => handleClick()}>New Question</button>
+          </>
+        )
+      }
+    </div>
+
   )
 }
 
