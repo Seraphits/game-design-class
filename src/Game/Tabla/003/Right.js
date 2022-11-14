430c36ca393edbd722a07d207c9afc666d17e495
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-11-13 22:29:07 -0500

    Right Screen

--- src/Game/Tabla/003/Right.js
+++ src/Game/Tabla/003/Right.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import { SubmitButton } from './Button.styled';
 import { QuestionComp } from './Question';
+import { Results, ResultsText } from './QuestionStyled';
 
 function Right() {
   const [showQuestion, setShowQuestion] = useState(false);
@@ -13,10 +15,10 @@ function Right() {
         {showQuestion ? (
           <QuestionComp/>
         ) : (
-          <>
-            Right
-            <button onClick={() => handleClick()}>New Question</button>
-          </>
+          <Results>
+            <ResultsText>Right</ResultsText>
+              <SubmitButton onClick={() => handleClick()}>New Question</SubmitButton>
+          </Results>
         )
       }
     </div>
