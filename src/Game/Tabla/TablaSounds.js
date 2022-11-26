bdf578c38332c7de9c120b8e7140829c6440d074
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-11-26 17:52:46 -0500

    Sounds renames

---
+++ src/Game/Tabla/TablaSounds.js
@@ -0,0 +1,28 @@
+const TablaSounds = [
+  {
+    "english": "GHE",
+    "hindi": "",
+    "spoken": "Spoken_GHE.mp3",
+    "played": "Dagga_GHE.mp3",
+    "drum": "Dagga"
+  },
+  {
+    "english": "GHI",
+    "hindi": "",
+    "spoken": "Spoken_GHI.mp3",
+    "played": "Dagga_GHI.mp3",
+    "drum": "Dagga"
+  },{
+    "english": "KA",
+    "hindi": "",
+    "spoken": "Spoken_KA.mp3",
+    "played": "Dagga_KA.mp3",
+    "drum": "Dagga"
+  },{
+    "english": "KATH",
+    "hindi": "",
+    "spoken": "Spoken_KATH.mp3",
+    "played": "Dagga_KATH.mp3",
+    "drum": "Dagga"
+  }
+]
--- src/Pages/Sounds.js
+++ src/Pages/Sounds.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TablaSounds from '../Game/Tabla/TablaSounds';
 
 export const Sounds = () => {
+  const [sounds, setSounds] = useState(TablaSounds);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [ currentSound, SetCurrentSound ] = useState(TablaSounds[0])
   return (
     <div>
       <h1>Sounds</h1>
-
+      <audio src='/Sounds/Tabla/Dagga_GHE.mp3'/>
+      {/* <Player/> */}
     </div>
   )
 }
