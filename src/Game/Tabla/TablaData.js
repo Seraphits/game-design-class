b7ce8bc18365c58467d9931b3a2d1ee872645a2c
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-11-27 22:56:16 -0500

    Added Tabla Sounds

--- src/Game/Tabla/TablaData.js
+++ src/Game/Tabla/TablaData.js
@@ -26,7 +26,64 @@ const Tabla_Sounds = [
     "spoken": "Spoken_KATH.mp3",
     "played": "Dagga_KATH.mp3",
     "drum": "Dagga"
-  }
+  },
+  {
+    "english": "CHAK",
+    "hindi": "",
+    "spoken": "Spoken_CHAK.mp3",
+    "played": "Tabla_CHAK.mp3",
+    "drum": "Tabla"
+  },
+  {
+    "english": "NA",
+    "hindi": "",
+    "spoken": "Spoken_NA.mp3",
+    "played": "Tabla_NA.mp3",
+    "drum": "Tabla"
+  },
+  {
+    "english": "RA",
+    "hindi": "",
+    "spoken": "Spoken_RA.mp3",
+    "played": "Tabla_RA.mp3",
+    "drum": "Tabla"
+  },
+  {
+    "english": "TE",
+    "hindi": "",
+    "spoken": "Spoken_TE.mp3",
+    "played": "Tabla_TE.mp3",
+    "drum": "Tabla"
+  },
+  {
+    "english": "TEE",
+    "hindi": "",
+    "spoken": "Spoken_TEE.mp3",
+    "played": "Tabla_TEE.mp3",
+    "drum": "Tabla"
+  },
+
+  {
+    "english": "TI",
+    "hindi": "",
+    "spoken": "Spoken_TI.mp3",
+    "played": "Tabla_TI.mp3",
+    "drum": "Tabla"
+  },
+  {
+    "english": "TIN",
+    "hindi": "",
+    "spoken": "Spoken_TIN.mp3",
+    "played": "Tabla_TIN.mp3",
+    "drum": "Tabla"
+  },
+  {
+    "english": "TUN",
+    "hindi": "",
+    "spoken": "Spoken_TUN.mp3",
+    "played": "Tabla_TUN.mp3",
+    "drum": "Tabla"
+  },
 ]
 
 export default Tabla_Sounds;
