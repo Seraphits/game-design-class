32688305a6f0f7737d3c9b00a36e3c0c447b0323
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-12-18 15:36:37 -0500

    2

--- src/UI/Buttons/Button.js
+++ src/UI/Buttons/Button.js
@@ -1,4 +1,5 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import className from "classnames";
 
 function Button ({
   children,
@@ -10,7 +11,22 @@ function Button ({
   outline,
   rounded
 }) {
-  return <button>{children}</button>;
+  const classes = className('px-3 py-1.5 border', {
+     'border-blue-600 bg-blue-500 text-white': primary,
+     'border-gray-900 bg-gray-800 text-white': secondary,
+     'border-green-600 bg-green-500 text-white': success,
+     'border-yellow-500 bg-yellow-400 text-white': warning,
+     'border-red-600 bg-red-500 text-white': danger,
+     'rounded-full': rounded,
+     'bg-white': outline,
+     'text-blue-600': outline && primary,
+     'text-gray-600': outline && secondary,
+     'text-green-600': outline && success,
+     'text-yellow-500': outline && warning,
+     'text-red-600': outline && danger,
+  });
+
+  return <button className={classes}>{children}</button>;
 };
 
 Button.propTypes = {
@@ -23,7 +39,6 @@ Button.propTypes = {
     if (count > 1) {
       return new Error('Only one of primary, secondary, success, warning, danger can be true')
     }
-    //
   }
 };
 
--- src/UI/Buttons/Buttons.js
+++ src/UI/Buttons/Buttons.js
@@ -8,22 +8,95 @@ export const Buttons = () => {
       <h1>Buttons</h1>
       <Grid>
         <div>
-          <p>name of button</p>
-          <Button primary>Button1</Button>
+          <div>
+            <p>name of button</p>
+            <Button primary>Button1</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button secondary>Button2</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button  success>Button3</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button warning>Button4</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button danger>Button4</Button>
+          </div>
         </div>
         <div>
-          <p>name of button</p>
-          <Button secondary>Button2</Button>
+          <div>
+            <p>name of button</p>
+            <Button primary rounded>Button1</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button secondary rounded>Button2</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button  success rounded>Button3</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button warning rounded>Button4</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button danger rounded>Button4</Button>
+          </div>
         </div>
         <div>
-          <p>name of button</p>
-          <Button>Button3</Button>
+          <div>
+            <p>name of button</p>
+            <Button primary outline>Button1</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button secondary outline>Button2</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button  success outline>Button3</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button warning outline>Button4</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button danger outline>Button4</Button>
+          </div>
         </div>
         <div>
-          <p>name of button</p>
-          <Button>Button4</Button>
+          <div>
+            <p>name of button</p>
+            <Button primary rounded outline>Button1</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button secondary rounded outline>Button2</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button  success rounded outline>Button3</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button warning rounded outline>Button4</Button>
+          </div>
+          <div>
+            <p>name of button</p>
+            <Button danger rounded outline>Button4</Button>
+          </div>
         </div>
       </Grid>
     </Wrapper>
   )
 }
+
