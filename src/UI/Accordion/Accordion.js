67a35c2d7552a5ec5f635d09a1bafb52ca946b1c
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-12-19 17:42:19 -0500

    1

--- src/UI/Accordion/Accordion.js
+++ src/UI/Accordion/Accordion.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { Wrapper } from '../Utility'
+import React, { useState } from 'react'
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go'
 
-export const Accordion = () => {
+export const Accordion = ({items}) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1)
+
+  const handleClick = (nextIndex) => {
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1)
+    } else {
+      setExpandedIndex(nextIndex);
+    }
+  }
+
+  const reneredItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = <span>
+      {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+    </span>
+    return (
+      <div key={item.id}>
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className='border-b p-5'>{item.content} </div>}
+      </div>
+    )
+  });
+
   return (
-    <Wrapper>
-      <h1>Accordion</h1>
-    </Wrapper>
+    <div className="border-b border-t rouneded" >
+      {reneredItems}
+    </div>
   )
 }
