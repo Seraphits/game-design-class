da1fcf8eca21f660f780ba231792000fff0a6fb4
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-12-20 14:03:57 -0500

    1

--- src/UI/DropDown/DropDown.js
+++ src/UI/DropDown/DropDown.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const DropDown = () => {
+export const DropDown = ({options, selection, onSelect}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+  const handleOptionClick = (option) => {
+    setIsOpen(false); // Close dropdown
+    onSelect(option); //option clickes
+  }
+
+  let content = 'Select...';
+  if (selection) {
+    content = selection.label;
+  }
+
+  const renderedOptions = options.map((option) => {
+    return (
+    <div onClick={() => handleOptionClick(option) } key={option.value} >
+      {option.label}
+    </div>
+    )})
   return (
-    <div>DropDown</div>
+    <div>
+      <div onClick={handleClick } >
+        {selection?.label || 'Select...'}
+      </div>
+      {isOpen && <div>{renderedOptions} </div>}
+    </div>
   )
 }
--- src/UI/DropDown/index.js
+++ src/UI/DropDown/index.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Wrapper } from '../Utility'
 import { DropDown } from './DropDown'
 
 export const DropDownPage = () => {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  }
+
+  const options = [
+    { label: "red", value: "red" },
+    { label: "blue", value: "blue" },
+    { label: "green", value: "green" },
+    { label: "yellow", value: "yellow" },
+  ]
+
   return (
     <Wrapper>
       <h1>Dropdown</h1>
-      <DropDown/>
+      <DropDown options={options} selection={selection} onSelect={handleSelect} />
     </Wrapper>
   )
 }
