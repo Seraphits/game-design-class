8cd3adfdff7847be77091da5e3a46bcdee3f60a6
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-12-23 11:19:11 -0500

    1

--- src/UI/Table/index.js
+++ src/UI/Table/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Wrapper } from '../Utility'
+import { Table } from './table'
 
 export const TablePage = () => {
   return (
     <Wrapper>
       <div>TablePage</div>
+      <Table/>
     </Wrapper>
 
   )
