d65bc16a0f05841bf8ba6559e38360bc1aeedebb
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-12-27 14:36:01 -0500

    5

--- src/AddingData/CarList/001/CarSearch.js
+++ src/AddingData/CarList/001/CarSearch.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { changeSearchTerm } from '../../../store'
 
 export const CarSearch = () => {
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => {
+    return state.cars.searchTerm;
+  })
+
+  const handleSearchTermChange = (event) => {
+    dispatch(changeSearchTerm(event.target.value));
+  }
   return (
-    <div>CarSearch</div>
+    <div className='list-header'>
+      <h3 className='title is-3'>Car List</h3>
+      <div className='search field is-horizontal' >
+        <label className='label'>Search</label>
+        <input
+        className='input'
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+        />
+      </div>
+    </div>
   )
 }
--- src/store/Slices/Cars/formSlices.js
+++ src/store/Slices/Cars/formSlices.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addCar } from "./carsSlices";
 
 const formSlice = createSlice({
   name: 'form',
@@ -13,6 +14,12 @@ const formSlice = createSlice({
     changeCost(state, action) {
       state.cost = action.payload;
     }
+  },
+  extraReducers(builer) {
+    builer.addCase(addCar, (state, action) => {
+      state.name = '';
+      state.cost = 0;
+    });
   }
 })
 
